96cba46498a333864e93f6bde10ee7e5365695cf
Tony Liu -X (tonyliu3 - AN HUI WANG XIN HENG TIAN RUAN JIAN YOU XIAN GONG SI at Cisco) <tonyliu3@cisco.com>
2024-04-25 16:55:47 +0800

    init project

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import com.example.demo.dao.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author DiiD
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public ResponseEntity<Object> Get(){
+        return new ResponseEntity<>("test", HttpStatus.OK);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<Object> getUser() {
+        User user = new User("liu", 1);
+        System.out.println(user.toString());
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+}
